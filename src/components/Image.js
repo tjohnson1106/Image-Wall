e788bcf4a03cdc936c910b917f47aa9ab5231fad
Thomas Johnson <tjohnson1106@gmail.com>
2018-12-24 16:54:49 -0500

    implemted photo props in image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 class Image extends Component {
   render() {
     const post = this.props.post;
-    return <figure className="figure" />;
+    return (
+      <figure className="figure">
+        <img className="image" src={post.imageLink} alt={post.description} />
+      </figure>
+    );
   }
 }
 
