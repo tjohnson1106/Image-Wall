7c9d7d745e5c00215eeaab84550963bf7459ecf4
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-01 15:24:45 -0500

    created initial root reducer

---
+++ src/data/posts.js
@@ -0,0 +1,22 @@
+const posts = [
+  {
+    id: 0,
+    description: "phones",
+    imageLink:
+      "https://images.pexels.com/photos/821754/pexels-photo-821754.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 1,
+    description: "underwater",
+    imageLink:
+      "https://images.pexels.com/photos/1677275/pexels-photo-1677275.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 2,
+    description: "spiral",
+    imageLink:
+      "https://images.pexels.com/photos/247676/pexels-photo-247676.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
+];
+
+export default posts;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
+import rootReducer from "./redux/reducer";
 import Main from "./components/Main";
 import "./styles/stylesheet.css";
 import * as serviceWorker from "./serviceWorker";
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-  <Router>
-    <Main />
-  </Router>,
+  <Provider>
+    <Router>
+      <Main />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
