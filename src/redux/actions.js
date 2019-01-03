503e2ea2acf938a3aaf430a1881cafa4dbb1913a
Thomas Johnson <tjohnson1106@gmail.com>
2019-01-03 15:52:01 -0500

    created start adding post action to dispatch firebase database

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,25 @@
+import { database } from "../config/database";
+
 // all action are mapped to props via bindActionCreator
 
 // 010320191300 Props are currently being passed in to Single component
 
 // remove
 // index points to index number in post array
+
+export function startAddingPost(post) {
+  return (dispatch) => {
+    return database
+      .ref("posts")
+      .update({
+        [post.id]: post
+      })
+      .then(() => {
+        dispatch(addPost(post));
+      });
+  };
+}
+
 export function removePost(index) {
   return {
     type: "REMOVE_POST",
